6f88e66a4734489aa2b54f72a2c5e82a81c20ae6
Jesse Game-Brown <jesse-git@istheguy.com>
2017-06-06 20:05:46 -0400

    file restructuring

---
+++ src/test/java/CharacterTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class CharacterTest
+{
+	@Test
+	public void testCanSetName(){
+	
+		Character mainCharacter = new Character();
+		mainCharacter.setName("Jandrew");
+		
+	}
+}
